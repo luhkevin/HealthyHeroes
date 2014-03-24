56d26266d4268488bd46b06ef1ee5a6ac0bb0aac
Kevin Lu <luhkevin@gmail.com>
2014-03-24 19:24:26 -0400

    Ingre, Prod

--- src/com/example/healthyheroes/ProductActivity.java
+++ src/com/example/healthyheroes/ProductActivity.java
@@ -1,17 +1,10 @@
 package com.example.healthyheroes;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-public class ProductActivity extends ActionBarActivity {
+public class ProductActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {

e31bbca2d2d203cfb99925ad1355aad9212d4a38
Alex Little <amlittle042@gmail.com>
2014-03-24 19:32:43 -0400

    Added buttons to activity_home layout

--- src/com/example/healthyheroes/HomeActivity.java
+++ src/com/example/healthyheroes/HomeActivity.java
@@ -9,14 +9,14 @@ public class HomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
     

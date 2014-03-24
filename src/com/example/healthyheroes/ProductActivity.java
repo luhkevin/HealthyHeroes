e434d4f110668bfc66730d2d74dc21dc83fc8b41
Alex Little <amlittle042@gmail.com>
2014-03-24 19:07:23 -0400

    Actually adding new files

---
+++ src/com/example/healthyheroes/ProductActivity.java
@@ -0,0 +1,29 @@
+package com.example.healthyheroes;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class ProductActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_product);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.product, menu);
+		return true;
+	}
+}

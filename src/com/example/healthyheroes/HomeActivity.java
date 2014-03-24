2a8303a58131da915d5e4c831a8744d70af46ecb
Kevin Lu <luhkevin@gmail.com>
2014-03-24 19:33:07 -0400

    added button code

--- src/com/example/healthyheroes/HomeActivity.java
+++ src/com/example/healthyheroes/HomeActivity.java
@@ -2,10 +2,12 @@ package com.example.healthyheroes;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class HomeActivity extends Activity {
-
+	public static final int IngredientActivity_ID = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,4 +22,15 @@ public class HomeActivity extends Activity {
         return true;
     }
     
+    public void onCreateSessionButton(View v) {
+    	Intent i = new Intent(this, IngredientActivity.class);
+    	startActivityForResult(i, IngredientActivity_ID);
+    }
+    
+    /*
+    public void onViewSessionButton(View v) {
+    	Intent i = new Intent(this, ViewSessionActivity.class);
+    	startActivityForResult(i, ViewSessionActivity_ID);
+    }
+    */
 }

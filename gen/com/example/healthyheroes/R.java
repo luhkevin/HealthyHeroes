db11d4832f01dff5d889913870776602c0542f9f
Alex Little <amlittle042@gmail.com>
2014-04-01 00:56:45 -0400

    Retweaked selling layout to use ListView, deleted SellingProductView

--- gen/com/example/healthyheroes/R.java
+++ gen/com/example/healthyheroes/R.java
@@ -29,7 +29,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080010;
+        public static final int action_settings=0x7f080013;
         public static final int button1=0x7f080000;
         public static final int button2=0x7f080001;
         public static final int ingredeint_add_button=0x7f080006;
@@ -39,13 +39,16 @@ public final class R {
         public static final int ingredient_price=0x7f080003;
         public static final int ingredient_quantity=0x7f080004;
         public static final int linearLayout1=0x7f08000e;
+        public static final int linearLayout2=0x7f08000f;
+        public static final int listView1=0x7f080012;
+        public static final int moneyTotal=0x7f080011;
         public static final int product_add_button=0x7f08000c;
         public static final int product_back_button=0x7f08000b;
         public static final int product_finish_button=0x7f08000d;
         public static final int product_name=0x7f080008;
         public static final int product_price=0x7f080009;
         public static final int product_quantity=0x7f08000a;
-        public static final int sellingProductView1=0x7f08000f;
+        public static final int textView1=0x7f080010;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;

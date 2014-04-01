4b89dadf07bc56c99a71b1d10672b6d39131d91d
Alex Little <amlittle042@gmail.com>
2014-04-01 00:20:12 -0400

    Added more to the SellingProductView skeleton

--- src/com/example/healthyheroes/SellingProductView.java
+++ src/com/example/healthyheroes/SellingProductView.java
@@ -3,13 +3,17 @@ package com.example.healthyheroes;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class SellingProductView extends View {
 
 	private Session currentSession;
 	private ArrayList<FoodItem> productList;
+	private Paint p;
 	
 	public SellingProductView(Context context, Session s) {
 		super(context);
@@ -21,11 +25,26 @@ public class SellingProductView extends View {
 	}
 	private void init(Session s) {
 			currentSession = s;
+			p = new Paint();
 			//TODO initialize productList
 	}
 	
+	@Override
+	public void onDraw(Canvas c) {
+		// TODO draw the products
+	}
 	
-	
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		int action = e.getAction(); // down move or up
+		if (action == MotionEvent.ACTION_UP) 
+		{
+			//TODO figure out if a + or - has been pressed
+		}
+		
+		
+		return true; //handled correctly
+	}
 	
 
 }

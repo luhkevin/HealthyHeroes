4b1c7e7f979c3d4546f8486d6fa8645229450691
Kevin Lu <luhkevin@gmail.com>
2014-04-08 10:16:51 -0400

    Test written

--- src/com/example/healthyheroes/HealthyHeroesTests.java
+++ src/com/example/healthyheroes/HealthyHeroesTests.java
@@ -7,8 +7,45 @@ import org.junit.Test;
 public class HealthyHeroesTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void FoodItemtest() {
+		FoodItem fd_apple = new FoodItem("Apple", (double) 1.0, 20);
+		FoodItem fd_orange = new FoodItem("Orange", (double) 1.25, 20, 10);
+		
+		
+		for(int i = 0; i < 10; i++) {
+			fd_apple.incrementNumberSold();
+			fd_orange.incrementNumberSold();
+		}
+		assertEquals(fd_apple.getNumberSold(), 10);
+		assertEquals(fd_orange.getNumberSold(), 20);
+		assertEquals(fd_apple.limitReached(), false);
+		assertEquals(fd_orange.limitReached(), true);
+		
+		for(int i = 0; i < 10; i++) {
+			fd_apple.incrementNumberSold();
+			fd_orange.incrementNumberSold();
+		}
 
+		assertEquals(fd_apple.getNumberSold(), 20);
+		assertEquals(fd_orange.getNumberSold(), 20);
+		assertEquals(fd_apple.limitReached(), true);
+		assertEquals(fd_orange.limitReached(), true);
+
+		for(int i = 0; i < 10; i++) {
+			fd_apple.decrementNumberSold();
+			fd_orange.decrementNumberSold();
+		}
+		assertEquals(fd_apple.getNumberSold(), 10);
+		assertEquals(fd_orange.getNumberSold(), 10);
+
+		for(int i = 0; i < 20; i++) {
+			fd_apple.decrementNumberSold();
+			fd_orange.decrementNumberSold();
+		}
+
+		assertEquals(fd_apple.getNumberSold(), 0);
+		assertEquals(fd_orange.getNumberSold(), 0);
+		assertEquals(fd_apple.limitReached(), true);
+		assertEquals(fd_orange.limitReached(), true);
+	}
 }
